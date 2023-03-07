c1ecaeccbc5beba2a5c3785fbf8e8a859f2ec0c1
rlawlsdk.1127 <rlawlsdk.1127@daum.net>
2023-03-08 00:18:26 +0900

    keyword: do not this Mistake5

--- app/src/main/java/com/example/coroutinepractice/mistakes/Mistake5.kt
+++ app/src/main/java/com/example/coroutinepractice/mistakes/Mistake5.kt
@@ -5,7 +5,6 @@ import android.os.PersistableBundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.viewModelFactory
 
 class Mistake5Activity: ComponentActivity() {
 
@@ -17,13 +16,21 @@ class Mistake5Activity: ComponentActivity() {
 
         // lifecycleScope는 액티비티가 destroy될 때 취소되므로 누수가 발생하지 않음
         // 이 점이 문제인데 lifecycleScope 액티비티가  destroy될 때 같이 destroy되므로
-        // 디바이스를 돌린다거나 다크 모드로 변경하는 등 구성 변경이 일어난다거나
-        // 버튼을 클릭하면 ui에 데이터를 업데이트 혹은 네트워크 call을 할때 구성변경이 일어나면
+        // 디바이스를 돌린다거나 다크 모드로 변경하는 등
+        // 버튼을 클릭하면 ui에 데이터를 업데이트 혹은 네트워크 call을 하던 도중에 구성변경이 발생하면 취소되길 원치 않았지만 취소된다.
+        // 이러한 이유로 코루틴 scope를 viewmodel에서 생성해야한다.
+        // 액티비티 혹은 프래그먼트의 생명주기보다 오래 살아있는 viewModel scope가 있는 이유이기도 하다.
         /*button.setOnClickListener {
             lifecycleScope.launch {
                 viewModel.postValueApi()
             }
         }*/
+
+        // 버튼을 클릭해서 네트워크 코드를 초기화해도 이제 액티비티가 아닌 뷰모델의 라이프사이클에 종속되기 때문에
+        // 구성 변경이 발생하면 뷰모델은 아직 살아있기 때문에 네트워크 call은 계속 실행된다.
+        button.setOnClickListener {
+            viewModel.postValueApi()
+        }
     }
 
 }
--- app/src/main/java/com/example/coroutinepractice/mistakes/Mistake5ViewModel.kt
+++ app/src/main/java/com/example/coroutinepractice/mistakes/Mistake5ViewModel.kt
@@ -1,11 +1,26 @@
 package com.example.coroutinepractice.mistakes
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 
 class Mistake5ViewModel: ViewModel() {
+   // 뷰모델보다 더 오래 살아있는 코루틴이 필요할 경우 일반적인 coroutineScope와 원하는 디스패처를 쓰면된다.
+    val scope = CoroutineScope(Dispatchers.Main)
+    // 이렇게 쓸 경우 더 이상 필요하지 않을 때 scope를 취소하는 것을 신경쓰면서 사용해야한다.
 
-    suspend fun postValueApi() {
+    /*suspend fun postValueApi() {
         delay(10000L)
+    }*/
+
+    // suspend 키워드 대신 간단하게 viewmodel lifecycle scope에서 launch하고
+    // 내부에 네트워크 코드 등을 넣어주면 된다.
+    // 이렇게 하면 suspend 함수가 아니므로 mainActivity에서 lifecycle scope를 사용할 필요가 없다.
+
+    fun postValueApi() {
+        /*viewModelScope.launch {
+            delay(10000L)
+        }*/
     }
 }
